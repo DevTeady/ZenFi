f83bc194f142f7a5b1b01e7c1489ef4620bfd0ff
David T Eady <daviteady@gmail.com>
2022-02-22 23:13:20 +0600

    Doing little installer enhancement

--- termux_installer.sh
+++ termux_installer.sh
@@ -1,9 +1,19 @@
 #!/data/data/com.termux/files/usr/bin/bash
+
 pkg install -y root-repo 
-pkg install -y git tsu python wpa-supplicant pixiewps iw
+pkg install -y git tsu python wpa-supplicant pixiewps iw python3 python3-pip
 
 git clone --depth 1 https://github.com/DevTeady/ZenFi .
 
 chmod +x zenfi.py
 
 printf "###############################################\n#  All done! Now you can run ZenFi with\n#   sudo python zenfi.py -i wlan0 -K\n#\n#  To update, run\n#   (git pull)\n###############################################\n"
+
+printf "Do you want to run the ZenFi script? [Y/n]"
+read user
+if [[ $user == "Y" || $user == "y" ]]; then
+  sudo python3 zenfi.py -i wlan0 -K
+else
+  printf "Thank You for using!\nexiting...\n"
+  exit 1
+fi
